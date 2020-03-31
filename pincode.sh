ea37c26f47bc5bceded61db78a5ee2f4386f69f8
pratikshad16 <dhagepratiksha16@gmail.com>
2020-03-31 09:21:42 +0530

    added usercase4

--- pincode.sh
+++ pincode.sh
@@ -2,7 +2,7 @@
 #!/bin/bash -x
 echo "enter the pincode:" pincode
 read pincode
-pinpatt="[0-9]{6}$"
+pinpatt="^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$"
 if [[ $pincode =~ $pinpatt ]]
 then
 	echo "valid pincode"
